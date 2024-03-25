b3b11ae7e22f35402eff9b5238ef888c70d4743e
amsmyslov <a.m.smyslov@yandex.ru>
2024-03-25 21:10:05 +0300

    add logic

---
+++ src/main/java/org/example/db/RegistrationsRepository.java
@@ -0,0 +1,22 @@
+package org.example.db;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class RegistrationsRepository {
+    private Connection connection = null;
+
+    public RegistrationsRepository() {
+        connection = DbConnector.getInstance().getConnection();
+    }
+
+    public void addNew(Registration registration) throws Exception {
+        Statement statement = connection.createStatement();
+
+        String insertQuery = String.format("INSERT INTO registrations (team_name, number_of_participants, selected_task) VALUES ('%s',%d,%d);", registration.getTeamName(), registration.getNumberOfParticipants(), registration.getSelectedTask());
+
+        statement.executeUpdate(insertQuery);
+
+        statement.close();
+    }
+}
